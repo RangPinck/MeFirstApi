bda3f58948046ea8ddc423b111dc4e297489c3e0
BaX <chernoff.serg3i@yandex.ru>
2024-06-04 18:00:45 +0300

    process tests

---
+++ VCWebApi.xTest/UserControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VeterinarClinicApi.Interfaces;
+using VeterinarClinicApi.Repositories;
+
+namespace VCWebApi.xTest
+{
+    public class UserControllerTests
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        //public UserControllerTests()
+        //{
+        //    _userRepository = userRepository;
+        //    _mapper = mapper;
+        //}
+
+        //[Fact]
+        //public void Authorization_ReturnsOkResult()
+        //{
+        //    string email = "fedorova.margarita@gmail.com";
+        //    string password = "354621";
+        //    var okRes = _userRepository.Authorization(email, password);
+        //    Assert.IsType<OkObjectResult>(okRes);
+        //}
+    }
+}
