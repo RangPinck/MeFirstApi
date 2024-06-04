8bc35706b7257c3fed877c90a0f280b891ddb5e0
BaX <chernoff.serg3i@yandex.ru>
2024-06-04 15:20:19 +0300

    restart test

--- VeterinarClinicApi.Test/Controller/UserTest.cs
+++ VeterinarClinicApi.Test/Controller/UserTest.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using VeterinarClinicApi.Controllers;
+using VeterinarClinicApi.Dto;
+using VeterinarClinicApi.Interfaces;
+using VeterinarClinicApi.Models;
+using VeterinarClinicApi.Repositories;
+using Moq;
 
 namespace VeterinarClinicApi.Test.Controller
 {
+    
     public class UserTest
     {
+        [Fact]
+        public void UserController_Authorization_ReturnOk()
+        {
+            
+
+        }
     }
 }
