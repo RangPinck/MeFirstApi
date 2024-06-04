65cbb12b4cd16ed051c247bb98208d7309a53ba4
BaX <chernoff.serg3i@yandex.ru>
2024-06-04 16:09:25 +0300

    process testing

---
+++ VcWebApi.Test/Controllers/UserControllerTest.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using VeterinarClinicApi.Interfaces;
+
+namespace VcWebApi.Test.Controllers
+{
+    [TestClass]
+    public class UserControllerTest
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+    }
+}
--- VeterinarClinicApi.Test/Controller/UserTest.cs
+++ VeterinarClinicApi.Test/Controller/UserTest.cs
@@ -1,24 +1,45 @@
-﻿using AutoMapper;
-using FakeItEasy;
-using FluentAssertions;
+﻿using AutoFixture;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
 using VeterinarClinicApi.Controllers;
 using VeterinarClinicApi.Dto;
 using VeterinarClinicApi.Interfaces;
 using VeterinarClinicApi.Models;
-using VeterinarClinicApi.Repositories;
-using Moq;
 
 namespace VeterinarClinicApi.Test.Controller
 {
-    
+
     public class UserTest
     {
+        private Mock<IUserRepository> _userRepositoryMock;
+        private Fixture _fixture;
+        private UserController _userController;
+        private readonly IMapper _mapper;
+
+        public UserTest()
+        {
+            _fixture = new Fixture();
+            _userRepositoryMock = new Mock<IUserRepository>();
+        }
+
+
         [Fact]
         public void UserController_Authorization_ReturnOk()
         {
-            
 
+            var authUser = _fixture.Create<User>();
+            _userRepositoryMock.Setup(
+                repo => repo.Authorization("", "")
+                ).Returns(authUser);
+
+            _userController = new UserController(_userRepositoryMock.Object, _mapper);
+
+            var result = _userController.Authorization("", "");
+
+            var obj = result as ObjectResult;
+
+            Assert.Equal(200, obj.StatusCode);
         }
     }
 }
